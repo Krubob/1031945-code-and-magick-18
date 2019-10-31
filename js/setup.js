44e2b78b28734e1d5263d017894d6230285eccdc
KruBob <res.19@yandex.ru>
2019-10-31 19:12:49 +0300

    Добавляет модуль по работе с сервером данных

--- js/setup.js
+++ js/setup.js
@@ -45,55 +45,52 @@
     window.dom.wizardFireball.querySelector('input').value = randomFireballColor;
   });
 
-  var numberOfObj = 4;
-
-  var tmpData = {
-    WIZARD_NAMES: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
-    WIZARD_SURNAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
-    COAT_COLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
-    EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green']
-  };
-
-  // dom.setupElement.classList.remove('hidden');
-
-  var renderWizard = function () {
+  var renderWizard = function (wizard) {
     window.dom.wizardElement = window.dom.similarWizardTemplate.cloneNode(true);
 
-    window.dom.wizardElement.querySelector('.setup-similar-label').textContent = randomArr[i].name;
-    window.dom.wizardElement.querySelector('.wizard-coat').style.fill = randomArr[i].coatColor;
-    window.dom.wizardElement.querySelector('.wizard-eyes').style.fill = randomArr[i].eyesColor;
+    window.dom.wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    window.dom.wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    // window.dom.wizardElement.querySelector('.wizard-eyes').style.fill = randomArr[i].eyesColor;
 
     return window.dom.wizardElement;
   };
 
-  // создаем функцию для генерации объекта со случайными значениями
-  var createRandomObj = function () {
-    var randomObj = {}; // создаем пустой объект для записи случайных значений
-    var randomName = getRandomElement(tmpData.WIZARD_NAMES);
-    var randomSurname = getRandomElement(tmpData.WIZARD_SURNAMES);
-    randomObj.name = randomName + ' ' + randomSurname;
-    randomObj.coatColor = getRandomElement(tmpData.COAT_COLORS);
-    randomObj.eyesColor = getRandomElement(tmpData.EYES_COLORS);
-    return randomObj;
-  };
+  // работа с сервером
 
-  // создаем функцию для генерации масссива из N объектов со случайными значениями
-  var createArrOfObj = function () {
-    var arr = []; // создаем пустой массив для записи в него объектов
-    for (var i = 0; i < numberOfObj; i++) {
-      arr.push(createRandomObj());
+  var form = window.dom.setupElement.querySelector('.setup-wizard-form');
+  // cоздаем обработчик события отправки данных формы на сервер
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), function () { // отправка данных на сервер
+      window.dom.setupElement.classList.add('hidden'); // закрытие формы при успешной оотправке
+    }, errorHandler);
+    evt.preventDefault(); // отменяем действие формы по умолчанию
+  });
+
+  // добавляем обработчик успешной загрузки в отдельную переменную и отрисовываем магов
+  var successHandler = function (wizards) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < 4; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
     }
-    return arr;
+    window.dom.similarListElement.appendChild(fragment);
+    window.dom.setupElement.querySelector('.setup-similar').classList.remove('hidden');
   };
 
-  var randomArr = createArrOfObj(numberOfObj);
+  // добавляем обработчик ошибки в отдельную переменную и отрисовываем сообщение об ошибке в dom-элемент
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
 
-  window.dom.fragment = document.createDocumentFragment();
-  for (var i = 0; i < numberOfObj; i++) {
-    window.dom.fragment.appendChild(renderWizard(randomArr[i]));
-  }
-  window.dom.similarListElement.appendChild(window.dom.fragment);
-  window.dom.setupSimilar.classList.remove('hidden');
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(successHandler, errorHandler);
 
 })();
 
